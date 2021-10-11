b4214966f235aa85b1d82e6e9fe76bd75da69401
lchristiani44 <lore_chm@hotmail.com.ar>
2021-10-11 13:16:18 -0300

    basic node & express course completed 12/12

--- README.md
+++ README.md
@@ -1,18 +1,38 @@
 # [Introduction to the Basic Node and Express Challenges](https://www.freecodecamp.org/learn/apis-and-microservices/basic-node-and-express/)  
 ## 1/12: myApp.js modified to print "Hello World" on the console.  
-## 2/12 app methods  
+## 2/12 Start a working Express server  
+### app methods  
 - app.listen(port)  
 - app.METHOD(PATH, HANDLER): HANDLER is a function that Express calls when the route is matched. Handlers take the form function(req, res) {...}, where *req is the request object, and res is the response object*.  
+
+        app.get('/', MyHandler);  
     
-    app.get('/', MyHandler);  
-    
-    const MyHandler = (req, res) => {  
-    res.send("Hello Express");  
-    }  
-  
-## 3/12  
+        const MyHandler = (req, res) => {  
+        res.send("Hello Express");  
+        }  
+
+## 3/12 Serve an HTML file  
 - app.sendFile(PATH)  
 > relative path: views\index.html  
+    
     res.sendFile(__dirname + 'views\index.html');  
   
-## 4/12  
+## 4/12 Serve static assets  
+> stylesheets, scripts, images...  
+- Middleware function: express.static(path) using absolute path  
+> Middleware are functions that intercept route handlers, adding some kind of information. A middleware needs to be mounted using the method app.use(path, middlewareFunction). In app.use, the path argument is optional.  
+  
+## 5/12 Serve JSON on a Specific Route  
+
+## 6/12 Using .env File
+
+## 7/12 Add a root level middleware to handle all requests with a response  
+
+## 8/12 Middleware chains (create a time (using new Date()) server)  
+
+## 9/12 Express routing  
+
+## 10/12 Query parameter  
+
+## 11-12/12 Receiving data from a POST req (body-parser)
+
--- myApp.js
+++ myApp.js
@@ -1,8 +1,20 @@
-// ! Creating an Express app object:
 var express = require('express');
+var bodyParser = require('body-parser');
+
+// ! Creating an Express app object:
 var app = express();
 
-// 
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.path} - ${req.ip}`);
+    next();
+});
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+// var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+// app.use(bodyParser.json()); // ! De no estar, req.body = 'undefined'
+  
 // app.get('/', MyHandler);
 
 // const MyHandler = (req, res) => {
@@ -19,6 +31,56 @@ res.sendFile(__dirname + 'views\index.html');
 // res.sendFile('./views/index.html');
 });
 
+app.use('/public', express.static(__dirname + '/public'));
+
+// app.get('/json', (req, res) => {
+//     res.json({"message": "Hello json"});
+//   });
+
+app.get('/json', (req, res) => {
+    if (process.env.MESSAGE_STYLE === 'uppercase') {
+        res.json({"message": "HELLO JSON"});
+    } else {
+        res.json({"message": "Hello json"});
+    };
+});
+  
+app.get('/now', (req, res, next) => {
+    req.time = new Date().toString();
+    // console.log(req.time);
+    next();
+}, (req, res) => {
+    res.send({time: req.time});
+});
+  
+// Routing (params :)
+app.get('/:word/echo', (req, res, next) => {
+    res.send({echo: req.params.word})
+})
+  
+// Query parameter
+app.get('/name', (req, res, next) => {
+    let actual_req_URL = '/name?first=firstname&last=lastname'
+    // res.json({"name": req.query.first, req.query.last})
+    res.send({name: `${req.query.first} ${req.query.last}`})
+})
+  
+// Receiving data from a POST req
+// ? app.route('/name').get(handler).post(handler)
+  
+app.post('/name', (req, res) => {
+    res.send({name: `${req.body.first} ${req.body.last}`})
+})
+  
+// app.post('/name', (req, res, next) => {
+//   res.send({name: `${req.body.first} ${req.body.last}`})
+//   next()
+// })
+  
+// app.post('/name', urlencodedParser, (req, res) => {
+//   res.send({name: `${req.body.first} ${req.body.last}`})
+// })
+  
 
   
 
@@ -29,31 +91,4 @@ res.sendFile(__dirname + 'views\index.html');
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
